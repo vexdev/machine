01ff3f83b0478db0f3a93701bb6ab80ef3b8a9e7
Luca Vitucci <luca.vitucci@centralway.com>
2016-09-06 13:34:26 +0200

    Added christian

--- functions.sh
+++ functions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 function prmas {
-	stash pull-request master @piotr.spyra @victor.ivascu @hans.vanwesenbeeck @ciprian.ursu @iliyan.ivanov @ljubisa.stankovic @patryk.zabicki 
+	stash pull-request master @christian.reinhold @piotr.spyra @victor.ivascu @hans.vanwesenbeeck @ciprian.ursu @iliyan.ivanov @ljubisa.stankovic @patryk.zabicki 
 }
 function prrel {
 	echo "Enter the version of the release branch (release/xxxxx)"
 	read branch
-	stash pull-request release/$branch @piotr.spyra @victor.ivascu @hans.vanwesenbeeck @ciprian.ursu @iliyan.ivanov @ljubisa.stankovic @patryk.zabicki
+	stash pull-request release/$branch @christian.reinhold @piotr.spyra @victor.ivascu @hans.vanwesenbeeck @ciprian.ursu @iliyan.ivanov @ljubisa.stankovic @patryk.zabicki
 }
 function screenshot {
 	adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > screen.png
